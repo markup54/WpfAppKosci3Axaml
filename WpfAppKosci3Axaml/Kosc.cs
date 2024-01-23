ca3ae14e0485f35711e3b4e52206ce0238458759
markup54 <marzenakupka@tlen.pl>
2024-01-23 11:19:51 +0100

    Klasa kosc

--- WpfAppKosci3Axaml/Kosc.cs
+++ WpfAppKosci3Axaml/Kosc.cs
@@ -1,12 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace WpfAppKosci3Axaml
 {
-    internal class Kosc
+    public class Kosc:INotifyPropertyChanged
     {
+        private int _wartosc;
+        public int Wartosc
+        {
+            get
+            {
+                return _wartosc;
+            }
+            //get => _wartosc;
+            set
+            {
+                _wartosc = value;
+                if(PropertyChanged != null)
+                {
+                    PropertyChanged.Invoke(this,
+                        new PropertyChangedEventArgs(nameof(Wartosc)));
+                }
+
+            }
+        }
+        private bool _zaznaczona;
+        public bool Zaznaczona
+        {
+            get => _zaznaczona;
+            set
+            {
+                _zaznaczona = value;
+                if(PropertyChanged != null) { 
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(nameof(Zaznaczona)));
+                }
+            }
+        }
+
+        public Kosc()
+        {
+            Wartosc = 0;
+            Zaznaczona = false;
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
     }
 }
